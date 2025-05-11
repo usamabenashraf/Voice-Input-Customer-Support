c01acd04f7ca6caf31bb318ff5da920d6a07a740
usamabenashraf <84629673+usamabenashraf@users.noreply.github.com>
2025-05-11 16:03:33 +0500

    Update run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y ffmpeg
+python -m venv venv
+source venv/bin/activate
+python -m pip install --upgrade pip
+source venv/bin/activate
+pip install -r requirements.txt
