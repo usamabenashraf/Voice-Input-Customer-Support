ee2e5861d6d6f1633a807b24e168afcfb92935ca
usamabenashraf <84629673+usamabenashraf@users.noreply.github.com>
2025-05-11 16:04:22 +0500

    Delete .github/workflows/run_app.sh

--- .github/workflows/run_app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y ffmpeg
-python -m venv venv
-source venv/bin/activate
-python -m pip install --upgrade pip
-source venv/bin/activate
-pip install -r requirements.txt
